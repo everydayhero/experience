408545149023fe83462128274fbb4878c5f82d8c
Ellis Clayton <ellis@ellisclayton.info>
2016-08-19 15:26:21 +1000

    Add react-router-redux support

--- source/shared.js
+++ source/shared.js
@@ -1,8 +1,9 @@
+const { routerReducer } = require('react-router-redux')
 const { createStore } = require('redux')
 
 const defaultCreateLocals = ({ params }) => (params)
 
-const defaultStore = () => createStore((state) => state)
+const defaultStore = () => createStore((state) => state, { routing: routerReducer() })
 
 const ensureRoutes = (name) => {
   throw new Error(`No routes key was found on options passed to ${name}`)
