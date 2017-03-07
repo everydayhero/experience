1eda47093683df289876fa995f8dd2e323e07224
Luke Brooker <mail@lukebrooker.com>
2017-03-08 08:25:07 +1000

    Add a whitelist and remove the need for options

--- packages/stranger/src/things/component.js
+++ packages/stranger/src/things/component.js
@@ -1,36 +1,38 @@
-import { PropTypes } from 'react'
-import mapProps from 'recompose/mapProps'
-import getContext from 'recompose/getContext'
-import pipe from 'lodash/fp/pipe'
-import omit from 'lodash/omit'
+import React, { PropTypes } from 'react'
+import pickBy from 'lodash/pickBy'
 import merge from 'lodash/merge'
 import rug from '@edh/rug'
 import { addRule } from '../'
+import validAttr from './reactValidHtmlAttr.js'
 
-export const strangerComp = (styles = {}) =>
-  (Component = 'div', options = {}) => (
-    pipe(
-      mapProps(props => {
-        const {
-          traits,
-          styles: existingStyles,
-          ...rest
-        } = props
-        const stylesObj = typeof styles === 'function'
-          ? styles({ props: rest, traits: traits || rug })
-          : styles
-        const mergedStyles = existingStyles
-          ? merge({}, stylesObj, existingStyles)
-          : stylesObj
-        const className = addRule(mergedStyles)
-        return {
-          ...options.removeProps ? omit(rest, options.removeProps) : rest,
-          ...typeof Component !== 'string' && !options.cancelPassStyles && { styles: mergedStyles },
-          className
-        }
-      }),
-      getContext({ traits: PropTypes.object })
-  )(Component)
-)
+const comp = (styles = {}) => (Component = 'div') => {
+  const StrangerComponent = (props, { traits }) => {
+    const {
+      styles: existingStyles,
+      ...rest
+    } = props
+    const stylesObj = typeof styles === 'function'
+      ? styles({ props: rest, traits: traits || rug })
+      : styles
+    const mergedStyles = existingStyles
+      ? merge({}, stylesObj, existingStyles)
+      : stylesObj
+    const className = addRule(mergedStyles)
+    const propsToPass = Component.isStrange
+      ? { ...rest, styles: mergedStyles }
+      : pickBy(rest, validAttr(Component.propTypes))
+    return (
+      <Component
+        {...propsToPass}
+        className={className}
+      />
+    )
+  }
+  StrangerComponent.isStrange = true
+  StrangerComponent.contextTypes = {
+    traits: PropTypes.object
+  }
+  return StrangerComponent
+}
 
-export default strangerComp
+export default comp
