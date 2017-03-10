4c5d806945bcfb13d61c17f76855c6e5f21e498d
Luke Brooker <mail@lukebrooker.com>
2017-03-10 16:40:11 +1000

    Swap fancy (broken) flex box scrolling divs for fixed nav

--- packages/experience-docs/source/components/PageWrapper/index.js
+++ packages/experience-docs/source/components/PageWrapper/index.js
@@ -3,21 +3,11 @@ import {comp} from '@everydayhero/stranger'
 const PageWrapper = comp(({
   traits: {
     size,
-    measure,
     media
   }
 }) => ({
   [media.md]: {
-    position: 'fixed',
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
-    overflow: 'hidden',
-    display: 'flex',
-    justifyContent: 'flex-end',
-    flexWrap: 'wrap',
-    flexDirection: 'row-reverse'
+    paddingLeft: '14rem'
   }
 }))('div')
 
