7677ac8b5c904f58452759f9d6e7c34002f89688
Justin Marrington <justin@marrington.net>
2017-04-24 11:43:41 +1000

    Pull in logo module from languishing branch

---
+++ packages/ui-logo/source/index.js
@@ -0,0 +1,41 @@
+import React, {PropTypes} from 'react'
+import camelCase from 'lodash/camelCase'
+import SVGInline from 'react-svg-inline'
+import {comp} from '@everydayhero/stranger'
+import * as logos from '@everydayhero/brand-assets'
+
+const Logo = ({
+  type = 'logo',
+  padded = true,
+  inverted
+}) => {
+  const logoName = camelCase(type) + (inverted ? 'Inverted' : '')
+  const svg = logos[logoName]
+  return (
+    svg !== undefined &&
+    <LogoWrapper>
+      <SVGInline svg={svg} />
+    </LogoWrapper>
+  )
+}
+
+Logo.propTypes = {
+  type: PropTypes.oneOf([
+    'logo',
+    'mark',
+    'powered-by'
+  ]),
+  inverted: PropTypes.bool,
+  padded: PropTypes.bool
+}
+
+export default Logo
+
+// Styled components
+const LogoWrapper = comp(({
+  traits: {
+    size
+  }
+}) => ({
+  padding: size(4)
+}))('div')
