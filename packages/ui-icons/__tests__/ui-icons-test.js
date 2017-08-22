ba9bca23bb1ab3d9b94a96bd094c1c64a2343007
Brad Parker <hi@bradparker.com>
2017-08-22 11:08:33 +1000

    Test that conversion has removed all instances of key color

--- packages/ui-icons/__tests__/ui-icons-test.js
+++ packages/ui-icons/__tests__/ui-icons-test.js
@@ -30,6 +30,14 @@ readDir(SOURCE_DIR).then((files) => {
         assert.equal(wrapper.find('svg').attr('viewbox'), '0 0 32 32')
       })
 
+      it('has no fill values of #203a44, but rather currentColor', () => {
+        const subject = module[name]
+        const wrapper = render(React.createElement(subject))
+
+        assert.equal(wrapper.find('[fill=#203a44]').length, 0)
+        assert(wrapper.find('[fill=currentColor]').length > 0)
+      })
+
       describe('inline variant', () => {
         it('has a width and height of 1em', () => {
           const subject = module[`Inline${name}`]
