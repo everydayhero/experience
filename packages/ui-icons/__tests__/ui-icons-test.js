fdebe271c893d29e526cea3c89baec6bb78009fb
Brad Parker <hi@bradparker.com>
2017-08-22 10:38:56 +1000

    Update tests to run multiple assertions with only one dir read
    Update tests to assert on new inline varient

--- packages/ui-icons/__tests__/ui-icons-test.js
+++ packages/ui-icons/__tests__/ui-icons-test.js
@@ -1,32 +1,53 @@
-const fs = require('fs')
-const {resolve} = require('path')
-const {promisify} = require('util')
-const React = require('react')
+const {describe, it, run} = require('mocha')
 const assert = require('assert')
-const {describe, it} = require('mocha')
+
+const React = require('react')
+const fs = require('fs')
+const {iconName} = require('../utils')
+const {promisify} = require('util')
 const {render} = require('enzyme')
+const {resolve} = require('path')
 
 const readDir = promisify(fs.readdir)
-
 const SOURCE_DIR = resolve(__dirname, '../icons')
 
-describe('All UI Icons', () => {
-  it('render successfully', () => (
-    readDir(SOURCE_DIR).then((files) => (
-      files.forEach((path) => {
-        const subject = require(resolve(SOURCE_DIR, path))
+readDir(SOURCE_DIR).then((files) => {
+  files.forEach((path) => {
+    const name = iconName(path)
+    const module = require(resolve(SOURCE_DIR, path))
+
+    describe(`Icon ${name}`, () => {
+      it('renders successfully', () => {
+        const subject = module[name]
+
         assert(render(React.createElement(subject)))
       })
-    ))
-  ))
 
-  it('have the same and correct viewBox', () => (
-    readDir(SOURCE_DIR).then((files) => (
-      files.forEach((path) => {
-        const subject = require(resolve(SOURCE_DIR, path))
+      it('has a viewBox of 0 0 32 32', () => {
+        const subject = module[name]
         const wrapper = render(React.createElement(subject))
+
         assert.equal(wrapper.find('svg').attr('viewbox'), '0 0 32 32')
       })
-    ))
-  ))
+
+      describe('inline variant', () => {
+        it('has a width and height of 1em', () => {
+          const subject = module[`Inline${name}`]
+          const wrapper = render(React.createElement(subject))
+
+          assert.equal(wrapper.find('svg').attr('height'), '1em')
+          assert.equal(wrapper.find('svg').attr('width'), '1em')
+        })
+
+        it('has a style attribute with a vertical transform of 0.18em', () => {
+          const subject = module[`Inline${name}`]
+          const wrapper = render(React.createElement(subject))
+
+          assert.equal(wrapper.find('svg').attr('style'), 'transform:translate(0, 0.18em);')
+        })
+      })
+    })
+  })
+
+  run()
 })
